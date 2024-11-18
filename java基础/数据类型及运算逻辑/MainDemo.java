94b56af0631aaf8622fbdbc58caca7384b40e94b
Ordinary_imp <161730187+Ordinaryimp@users.noreply.github.com>
2024-11-18 13:11:19 +0800

    Add files via upload

---
+++ java基础/数据类型及运算逻辑/MainDemo.java
@@ -0,0 +1,18 @@
+package com.fourteeen;
+
+/**
+ * ClassName:MainDemo
+ * Package:com.fourteeen
+ * Description:
+ *
+ * @Author mzy
+ * @Create 2024/11/18 12:54
+ * @Version 1.0
+ */
+public class MainDemo {
+    public static void main(String[] args) {
+        for(int i = 0;i<args.length;i++){
+            System.out.println("hello:"+args[i]);
+        }
+    }
+}
